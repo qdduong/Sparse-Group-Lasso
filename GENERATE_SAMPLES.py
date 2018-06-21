a5d39af04f678bac9ea6c37fe6e678ee6b254184
qdduong <quangdien.duong@gmail.com>
2018-06-21 15:29:27 +0200

    Add files via upload

---
+++ GENERATE_SAMPLES.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 15:09:00 2018
+
+@author: Quang Dien Duong
+"""
+
+__author__ = 'Quang Dien Duong quangdien.duong[at]gmail.com'
+
+
+import setting as st
+import numpy as np
+import pickle
+
+np.random.seed(1000)
+N = 10
+
+X1 = np.linspace(0.0, 1.0, num=N, endpoint=False)
+X2 = np.linspace(0.0, 1.0, num=N, endpoint=False)
+X3 = np.linspace(0.0, 1.0, num=N, endpoint=False)
+
+X = []
+for x1 in X1:
+    for x2 in X2:
+        for x3 in X3:
+            X.append([x1,x2,x3])
+X = np.round(np.asarray(X),2)
+# =============================================================================
+# Generate knots
+# =============================================================================
+l1 = list(np.sort(np.random.uniform(size=34)))
+l2 = list(np.sort(np.random.uniform(size=34)))
+l3 = list(np.sort(np.random.uniform(size=34)))
+knots = np.array([l1,l2,l3]).T
+# =============================================================================
+# Generate samples 
+# =============================================================================
+Y = st.generate_GPD_samples(X)
+# =============================================================================
+# True gamma and sigma function 
+gamma0 = np.array([st.gamma_function(x) for x in X])
+sigma0 = np.array([st.sigma_function(x) for x in X])
+with open('gamma0_sigma0.pickle', 'wb') as handle:
+    pickle.dump(gamma0, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(sigma0, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# =============================================================================    
---
+++ setting.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 14:58:34 2018
+
+@author: Quang Dien Duong
+"""
+
+__author__ = 'Quang Dien Duong quangdien.duong[at]gmail.com'
+
+from scipy.stats import norm
+import numpy as np
+
+
+def generate_uniform_distribution_random_vector(n):
+    resu = np.random.uniform(size = n)
+    return resu
+
+def generate_Burr_distributed_samples(eta, lamda, tau , size):
+    assert eta > 0, "eta must be positive."
+    assert lamda > 0, "lamda must be positive."
+    assert tau > 0, "tau must be positive."
+    assert type(size) == int, "size must be an integer."
+    Fvec = generate_uniform_distribution_random_vector(size)
+    resu = eta*((1-Fvec)**(-1/lamda)-1)
+    resu = resu**(1/tau)
+    return resu
+
+# =============================================================================
+#       Generalized Pareto distribution
+# =============================================================================
+def sigma_function(x):
+    """
+        x: array-type. In our setup, x is a tridimensional vector, i.e. x = [x1, x2, x3] 
+    """
+    resu = 10 + 0.2*x[0] + 0.05*x[0]**2 + 0.5*x[1]
+    return resu
+    
+def gamma_function(x):
+    """
+        x: array-type. In our setup, x is a tridimensional vector, i.e. x = [x1, x2, x3] 
+    """
+    resu = (0.1 + 0.25*x[0] + 0.05*x[1] + norm.pdf((x[0]-0.05)/0.2) + norm.pdf((x[1]+0.05)/0.5))
+    return resu
+
+def generate_GPD_samples(x, default_seed=1660):
+    resu = []
+    for i in range(len(x)):
+        np.random.seed(default_seed+i)
+        gamma = gamma_function(x[i])
+        sigma = sigma_function(x[i])
+        u = np.random.uniform()
+        out = sigma/gamma*((1-u)**(-gamma)-1)
+        resu.append(out)
+    return resu
